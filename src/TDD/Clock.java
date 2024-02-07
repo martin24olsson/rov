bfd3785dbaaf61bbe1d8a8d7775f067ffef7018c
martin24olsson <128680764+martin24olsson@users.noreply.github.com>
2024-02-07 10:10:03 +0100

    a

---
+++ src/TDD/Clock.java
@@ -0,0 +1,12 @@
+package TDD;
+
+public class Clock {
+    public String changeMode(){
+        return null;
+    }
+
+    public String ready(){
+        return 
+    }
+
+}
---
+++ src/TDD/Main.java
@@ -0,0 +1,7 @@
+package TDD;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
