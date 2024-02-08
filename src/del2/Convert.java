7ac891665cd3869d56b4b37b4fb4fa7b9de0d930
martin24olsson <128680764+martin24olsson@users.noreply.github.com>
2024-02-08 12:41:48 +0100

    aaa

--- src/del2/Convert.java
+++ src/del2/Convert.java
@@ -6,22 +6,30 @@ public class Convert {
     private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#.#################");
 
     public String toDegrees(int deg, int min, int s) {
+        if(deg <0 || min<0 || s<0){
+            return "illegal";
+        }
+
         double nbr = deg + (min / 60.0) + (s / 3600.0);
-        return DECIMAL_FORMAT.format(nbr);
+        return DECIMAL_FORMAT.format(nbr);  // Format the result using DECIMAL_FORMAT
     }
 
-    public static String toDMS(String str){
+    public static String toDMS(String str) {
+        str = str.replace(',', '.');
+
         double dd = Double.parseDouble(str);
+        DECIMAL_FORMAT.format(dd);
 
         int d = (int) dd; //d is decimal and dd is decimal degrees
         double min = (dd-d) * 60;
         double minRoundedDouble = (int) min;
         double sec = (dd-d-(minRoundedDouble/60)) * 3600;
+        //This is equal to -> double sec = (min - (int) min) * 60;
 
         int minRounded = (int) min;
-        int secRounded = (int) sec;
+        int secRounded = (int) Math.round(sec);
 
-        String output = d + " " + minRounded +" "  + secRounded;
+        String output = d + " " + minRounded + " " + secRounded;
         return output;
     }
 }
--- src/del2/ConvertTest.java
+++ src/del2/ConvertTest.java
@@ -2,33 +2,56 @@ package del2;
 
 import org.junit.jupiter.api.Test;
 
-import static com.company.Rovar.enrov;
-import static del2.Convert.*;
 import static junit.framework.TestCase.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ConvertTest {
     private Convert convert = new Convert();
 
     @Test
     void toDegrees() {
-        assertEquals(convert.toDegrees(30, 15, 50), ("30,26388888888889"));
+        assertEquals(convert.toDegrees(30, 15, 36), ("30,26"));
     }
 
     @Test
     void toDMS() { //DMS = Degrees, Minutes, Seconds
-        assertEquals(convert.toDMS("30.26388888888889"), ("30 15 50"));
+        assertEquals(convert.toDMS("30,26"), ("30 15 36"));
     }
 
 
-    /*
+
     @Test
-    void random() {
-        String str = convert.toDegrees(16, 8, 4);
-        System.out.println(str);
-        String correctStr = str.replace(',', '.');
-        assertEquals(convert.toDMS(correctStr), ("16 8 4"));
-    }*/
+    void BVA1() {
+        assertEquals(convert.toDegrees(-1, 8, 4), ("illegal"));
+    }
+
+    @Test
+    void BVA2() {
+        String str = convert.toDegrees(12, 0, 14);
+        assertEquals(convert.toDMS(str), ("12 0 14"));
+    }
+
+    @Test
+    void BVA3() {
+        String str = convert.toDegrees(18, 3, 0);
+        assertEquals(convert.toDMS(str), ("18 3 0"));
+    }
+
+    @Test
+    void numbersAddUp() {
+        String str = convert.toDegrees(10, 9, 16);
+        str = str.replace(',', '.');
+        String str2 = convert.toDegrees(5, 6, 8);
+        str2 = str2.replace(',', '.');
+        String str3 = convert.toDegrees(15, 3, 4);
+        str3 = str3.replace(',', '.');
+
+        double addUpp = Double.parseDouble(str)
+                + Double.parseDouble(str2)
+                + Double.parseDouble(str3);
+
+        assertEquals(convert.toDMS(String.valueOf(addUpp)), ("30 18 28"));
+    }
+
 
 
 
