6d561646c0b792133e19807f8e571a24c88e226d
Nicklas02 <Nicklasc.ronaldo@gmail.com>
2024-02-08 14:33:43 +0100

    jrjr

--- src/TDD/Date.java
+++ src/TDD/Date.java
@@ -5,10 +5,21 @@ public class Date {
     private int theMonth = 1;
     private int theDay = 1;
 
-    protected void dateSet(int year, int month, int day) {
+    protected String dateSet(int year, int month, int day) {
+        if(year > 5000 || year < 0 ) {
+            return "invalid year";
+        }
+        if(month > 12 || month < 1) {
+            return "invalid month";
+        }
+        if(day > 31 || day < 1) {
+            return "invalid day";
+        }
         this.theYear = year;
         this.theMonth = month;
         this.theDay = day;
+
+        return "Success date change";
     }
 
     protected String showDate() {
